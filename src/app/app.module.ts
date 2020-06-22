4dabe146f776ee5e11dd772de2150b2381934886
SeungkeunHyun <mysuccess@daum.net>
2020-06-22 12:02:25 +0900

    added main toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+import { MainToolbarComponent } from './components/main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
